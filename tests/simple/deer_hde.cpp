a8eca310b41410a08c56a60a37898e92804d0bf2
oren <virtualoren@gmail.com>
2021-01-21 14:37:35 -0600

    Full mobilenet architecture. Fixed (non-zero) output

--- tests/simple/deer_hde.cpp
+++ tests/simple/deer_hde.cpp
@@ -29,45 +29,94 @@ int main() {
     tk::dnn::dataDim_t dim(1, 3, 192, 192, 1);
     tk::dnn::Network net(dim);
 
-    tk::dnn::Conv2d conv1(&net, 32, 3, 3, 2, 2, 1, 1, conv0_bin);
+    tk::dnn::Conv2d conv1(&net, 32, 3, 3, 2, 2, 1, 1, "test_deer_hde/layers/conv1.bin", true);
     tk::dnn::Activation relu1(&net, CUDNN_ACTIVATION_RELU);
 
-    // First group of depthwise
-    tk::dnn::Conv2d conv2(&net, 32, 3, 3, 1, 1, 1, 1, inverted_residual1[0], false, false, 32);
+    // First depthwise block
+    tk::dnn::Conv2d conv_dw_1(&net, 32, 3, 3, 1, 1, 1, 1, "test_deer_hde/layers/conv_dw_1.bin", true, false, 32);
     tk::dnn::Activation relu2(&net, CUDNN_ACTIVATION_RELU);
-    tk::dnn::Conv2d conv3(&net, 32, 3, 3, 2, 2, 1, 1, inverted_residual1[1], false, false, 32);
+    tk::dnn::Conv2d conv_pw_1(&net, 64, 1, 1, 1, 1, 0, 0, "test_deer_hde/layers/conv_pw_1.bin", true, false, 1);
     tk::dnn::Activation relu3(&net, CUDNN_ACTIVATION_RELU);
 
-    // Second group of depthwise
-    tk::dnn::Conv2d conv4(&net, 32, 3, 3, 1, 1, 1, 1, inverted_residual2[0], false, false, 32);
+    // Second depthwise block
+    tk::dnn::Conv2d conv_dw_2(&net, 64, 3, 3, 2, 2, 1, 1, "test_deer_hde/layers/conv_dw_2.bin", true, false, 64);
     tk::dnn::Activation relu4(&net, CUDNN_ACTIVATION_RELU);
-    tk::dnn::Conv2d conv5(&net, 32, 3, 3, 2, 2, 1, 1, inverted_residual2[1], false, false, 32);
+    tk::dnn::Conv2d conv_pw_2(&net, 128, 1, 1, 1, 1, 0, 0, "test_deer_hde/layers/conv_pw_2.bin", true, false, 1);
     tk::dnn::Activation relu5(&net, CUDNN_ACTIVATION_RELU);
 
-    // Third group of depthwise
-    tk::dnn::Conv2d conv6(&net, 32, 3, 3, 1, 1, 1, 1, inverted_residual3[0], false, false, 32);
+    // Third depthwise block
+    tk::dnn::Conv2d conv_dw_3(&net, 128, 3, 3, 1, 1, 1, 1, "test_deer_hde/layers/conv_dw_3.bin", true, false, 128);
     tk::dnn::Activation relu6(&net, CUDNN_ACTIVATION_RELU);
-    tk::dnn::Conv2d conv7(&net, 32, 3, 3, 2, 2, 1, 1, inverted_residual3[1], false, false, 32);
+    tk::dnn::Conv2d conv_pw_3(&net, 128, 1, 1, 1, 1, 0, 0, "test_deer_hde/layers/conv_pw_3.bin", true, false, 1);
     tk::dnn::Activation relu7(&net, CUDNN_ACTIVATION_RELU);
 
-    // Fourth group of depthwise
-    tk::dnn::Conv2d conv8(&net, 32, 3, 3, 1, 1, 1, 1, inverted_residual4[0], false, false, 32);
+    // Fourth depthwise block
+    tk::dnn::Conv2d conv_dw_4(&net, 128, 3, 3, 2, 2, 1, 1, "test_deer_hde/layers/conv_dw_4.bin", true, false, 128);
     tk::dnn::Activation relu8(&net, CUDNN_ACTIVATION_RELU);
-    tk::dnn::Conv2d conv9(&net, 32, 3, 3, 2, 2, 1, 1, inverted_residual4[1], false, false, 32);
+    tk::dnn::Conv2d conv_pw_4(&net, 256, 1, 1, 1, 1, 0, 0, "test_deer_hde/layers/conv_pw_4.bin", true, false, 1);
     tk::dnn::Activation relu9(&net, CUDNN_ACTIVATION_RELU);
 
-    // Fifth group of depthwise
-    tk::dnn::Conv2d conv10(&net, 32, 3, 3, 1, 1, 1, 1, inverted_residual5[0], false, false, 32);
+    // Fifth depthwise block
+    tk::dnn::Conv2d conv_dw_5(&net, 256, 3, 3, 1, 1, 1, 1, "test_deer_hde/layers/conv_dw_5.bin", true, false, 256);
     tk::dnn::Activation relu10(&net, CUDNN_ACTIVATION_RELU);
-    tk::dnn::Conv2d conv11(&net, 32, 3, 3, 2, 2, 1, 1, inverted_residual5[1], false, false, 32);
+    tk::dnn::Conv2d conv_pw_5(&net, 256, 1, 1, 1, 1, 0, 0, "test_deer_hde/layers/conv_pw_5.bin", true, false, 1);
     tk::dnn::Activation relu11(&net, CUDNN_ACTIVATION_RELU);
 
-    //Final layers 12
-    tk::dnn::Conv2d conv12(&net, 32, 1, 1, 1, 1, 0, 0, conv12_name);
+    // Sixth depthwise block
+    tk::dnn::Conv2d conv_dw_6(&net, 256, 3, 3, 2, 2, 1, 1, "test_deer_hde/layers/conv_dw_6.bin", true, false, 256);
     tk::dnn::Activation relu12(&net, CUDNN_ACTIVATION_RELU);
-    tk::dnn::Dense dense(&net, 2, dense_name);
+    tk::dnn::Conv2d conv_pw_6(&net, 512, 1, 1, 1, 1, 0, 0, "test_deer_hde/layers/conv_pw_6.bin", true, false, 1);
+    tk::dnn::Activation relu13(&net, CUDNN_ACTIVATION_RELU);
+
+    // Seventh depthwise block
+    tk::dnn::Conv2d conv_dw_7(&net, 512, 3, 3, 1, 1, 1, 1, "test_deer_hde/layers/conv_dw_7.bin", true, false, 512);
+    tk::dnn::Activation relu14(&net, CUDNN_ACTIVATION_RELU);
+    tk::dnn::Conv2d conv_pw_7(&net, 512, 1, 1, 1, 1, 0, 0, "test_deer_hde/layers/conv_pw_7.bin", true, false, 1);
+    tk::dnn::Activation relu15(&net, CUDNN_ACTIVATION_RELU);
+
+    // Eigth depthwise block
+    tk::dnn::Conv2d conv_dw_8(&net, 512, 3, 3, 1, 1, 1, 1, "test_deer_hde/layers/conv_dw_8.bin", true, false, 512);
+    tk::dnn::Activation relu16(&net, CUDNN_ACTIVATION_RELU);
+    tk::dnn::Conv2d conv_pw_8(&net, 512, 1, 1, 1, 1, 0, 0, "test_deer_hde/layers/conv_pw_8.bin", true, false, 1);
+    tk::dnn::Activation relu17(&net, CUDNN_ACTIVATION_RELU);
+
+    // Ninth depthwise block
+    tk::dnn::Conv2d conv_dw_9(&net, 512, 3, 3, 1, 1, 1, 1, "test_deer_hde/layers/conv_dw_9.bin", true, false, 512);
+    tk::dnn::Activation relu18(&net, CUDNN_ACTIVATION_RELU);
+    tk::dnn::Conv2d conv_pw_9(&net, 512, 1, 1, 1, 1, 0, 0, "test_deer_hde/layers/conv_pw_9.bin", true, false, 1);
+    tk::dnn::Activation relu19(&net, CUDNN_ACTIVATION_RELU);
+
+    // Tenth depthwise block
+    tk::dnn::Conv2d conv_dw_10(&net, 512, 3, 3, 1, 1, 1, 1, "test_deer_hde/layers/conv_dw_10.bin", true, false, 512);
+    tk::dnn::Activation relu20(&net, CUDNN_ACTIVATION_RELU);
+    tk::dnn::Conv2d conv_pw_10(&net, 512, 1, 1, 1, 1, 0, 0, "test_deer_hde/layers/conv_pw_10.bin", true, false, 1);
+    tk::dnn::Activation relu21(&net, CUDNN_ACTIVATION_RELU);
+
+    // Eleventh depthwise block
+    tk::dnn::Conv2d conv_dw_11(&net, 512, 3, 3, 1, 1, 1, 1, "test_deer_hde/layers/conv_dw_11.bin", true, false, 512);
+    tk::dnn::Activation relu22(&net, CUDNN_ACTIVATION_RELU);
+    tk::dnn::Conv2d conv_pw_11(&net, 512, 1, 1, 1, 1, 0, 0, "test_deer_hde/layers/conv_pw_11.bin", true, false, 1);
+    tk::dnn::Activation relu23(&net, CUDNN_ACTIVATION_RELU);
+
+    // Twelfth depthwise block
+    tk::dnn::Conv2d conv_dw_12(&net, 512, 3, 3, 2, 2, 1, 1, "test_deer_hde/layers/conv_dw_12.bin", true, false, 512);
+    tk::dnn::Activation relu24(&net, CUDNN_ACTIVATION_RELU);
+    tk::dnn::Conv2d conv_pw_12(&net, 1024, 1, 1, 1, 1, 0, 0, "test_deer_hde/layers/conv_pw_12.bin", true, false, 1);
+    tk::dnn::Activation relu25(&net, CUDNN_ACTIVATION_RELU);
+
+    // Twelfth depthwise block
+    tk::dnn::Conv2d conv_dw_13(&net, 1024, 3, 3, 1, 1, 1, 1, "test_deer_hde/layers/conv_dw_13.bin", true, false, 1024);
+    tk::dnn::Activation relu26(&net, CUDNN_ACTIVATION_RELU);
+    tk::dnn::Conv2d conv_pw_13(&net, 1024, 1, 1, 1, 1, 0, 0, "test_deer_hde/layers/conv_pw_13.bin", true, false, 1);
+    tk::dnn::Activation relu27(&net, CUDNN_ACTIVATION_RELU);
+
+    //Final layers 12
+    tk::dnn::Conv2d conv2d(&net, 32, 3, 3, 3, 3, 0, 0, "test_deer_hde/layers/conv2d.bin");
+    tk::dnn::Dense dense1(&net, 64, "test_deer_hde/layers/dense.bin");
     tk::dnn::Activation sigmoid1(&net, CUDNN_ACTIVATION_SIGMOID);
-    tk::dnn::Layer *header[1] = {&sigmoid1};
+    tk::dnn::Dense dense2(&net, 2, "test_deer_hde/layers/out.bin");
+    tk::dnn::Activation sigmoid2(&net, CUDNN_ACTIVATION_SIGMOID);
+    tk::dnn::Layer *header[1] = {&sigmoid2};
 
     net.print();
 
--- tests/simple/deer_hde.py
+++ tests/simple/deer_hde.py
@@ -1,10 +1,11 @@
 import os
 import errno
+import datetime
 
 import keras
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Input, Dense, Activation, Flatten, Dropout, ELU, Reshape, Lambda, Conv1D, Conv2D, DepthwiseConv2D
+from keras import layers
 from keras.layers.convolutional import Convolution2D, Convolution3D
 from keras.layers.pooling import MaxPooling2D, MaxPooling3D, AveragePooling3D
 from keras.models import Sequential, Model
@@ -12,7 +13,7 @@ from keras.layers import Cropping2D
 import struct
 from keras.models import Sequential, Model
 
-input_shape = (3, 192, 192)
+input_shape = (192, 192, 3)
 
 def bin_write(f, data):
     data = data.flatten()
@@ -21,23 +22,98 @@ def bin_write(f, data):
     f.write(bin)
 
 def create_model():
+    x = keras.applications.MobileNet(input_shape, 1, include_top=False, weights="imagenet")
+    x.layers[0]._name = "data"
+    output = x.output
+    output = layers.Conv2D(32, (3,3), strides=(2,2), use_bias=False, padding='valid')(output)
+    output = layers.Flatten()(output)
+    output = layers.Dense(64, activation="sigmoid")(output)
+    output = layers.Dense(2, activation="sigmoid", name="out")(output)
+    hde = keras.models.Model(x.input, output)
+    hde.load_weights("hde_deer.hdf5")
+    hde.summary()
 
+    return hde
+
+def create_compact_model():
     hde = keras.Sequential()
     hde.add(keras.Input(shape=input_shape, name="data"))
-    hde.add(Conv2D(32, (3,3), strides=(2,2), padding="same", data_format="channels_first"))
-    hde.add(DepthwiseConv2D((3,3), strides=(1,1), padding="same", data_format="channels_first"))
-    hde.add(DepthwiseConv2D((3,3), strides=(2,2), padding="same", data_format="channels_first"))
-    hde.add(DepthwiseConv2D((3,3), strides=(1,1), padding="same", data_format="channels_first"))
-    hde.add(DepthwiseConv2D((3,3), strides=(2,2), padding="same", data_format="channels_first"))
-    hde.add(DepthwiseConv2D((3,3), strides=(1,1), padding="same", data_format="channels_first"))
-    hde.add(DepthwiseConv2D((3,3), strides=(2,2), padding="same", data_format="channels_first"))
-    hde.add(DepthwiseConv2D((3,3), strides=(1,1), padding="same", data_format="channels_first"))
-    hde.add(DepthwiseConv2D((3,3), strides=(2,2), padding="same", data_format="channels_first"))
-    hde.add(DepthwiseConv2D((3,3), strides=(1,1), padding="same", data_format="channels_first"))
-    hde.add(DepthwiseConv2D((3,3), strides=(2,2), padding="same", data_format="channels_first"))
-    hde.add(Conv2D(32, (3,3), strides=(1,1), padding="same", data_format="channels_first"))
-    hde.add(Flatten())
-    hde.add(Dense(2, activation="sigmoid", name="out"))
+    hde.add(layers.Conv2D(32, (3,3), strides=(2,2), padding="same", name="conv2d_1"))
+
+    hde.add(layers.DepthwiseConv2D((3,3), strides=(1,1), padding="same", name="conv_dw_2_dwconv2d"))
+    hde.add(layers.BatchNormalization(name="conv_dw_2_dwbn"))
+    hde.add(layers.ReLU(6, name="conv_dw_2_dwrelu"))
+    hde.add(layers.Conv2D(64, (1,1), padding="same", use_bias=False, strides=(1,1), name="conv_dw_2_pwconv2d"))
+    hde.add(layers.BatchNormalization(name="conv_dw_2_pwbn"))
+    hde.add(layers.ReLU(6, name="conv_dw_2_pwrelu"))
+
+    hde.add(layers.DepthwiseConv2D((3,3), strides=(2,2), padding="same"))
+    hde.add(layers.BatchNormalization(name="conv_dw_3_dwbn"))
+    hde.add(layers.ReLU(6, name="conv_dw_3_dwrelu"))
+    hde.add(layers.Conv2D(128, (1,1), padding="same", use_bias=False, strides=(1,1), name="conv_dw_3_pwconv2d"))
+    hde.add(layers.BatchNormalization(name="conv_dw_3_pwbn"))
+    hde.add(layers.ReLU(6, name="conv_dw_3_pwrelu"))
+
+    hde.add(layers.DepthwiseConv2D((3,3), strides=(1,1), padding="same"))
+    hde.add(layers.BatchNormalization(name="conv_dw_4_dwbn"))
+    hde.add(layers.ReLU(6, name="conv_dw_4_dwrelu"))
+    hde.add(layers.Conv2D(128, (1,1), padding="same", use_bias=False, strides=(1,1), name="conv_dw_4_pwconv2d"))
+    hde.add(layers.BatchNormalization(name="conv_dw_4_pwbn"))
+    hde.add(layers.ReLU(6, name="conv_dw_4_pwrelu"))
+
+    hde.add(layers.DepthwiseConv2D((3,3), strides=(2,2), padding="same"))
+    hde.add(layers.BatchNormalization(name="conv_dw_5_dwbn"))
+    hde.add(layers.ReLU(6, name="conv_dw_5_dwrelu"))
+    hde.add(layers.Conv2D(256, (1,1), padding="same", use_bias=False, strides=(1,1), name="conv_dw_5_pwconv2d"))
+    hde.add(layers.BatchNormalization(name="conv_dw_5_pwbn"))
+    hde.add(layers.ReLU(6, name="conv_dw_5_pwrelu"))
+
+    hde.add(layers.DepthwiseConv2D((3,3), strides=(1,1), padding="same"))
+    hde.add(layers.BatchNormalization(name="conv_dw_6_dwbn"))
+    hde.add(layers.ReLU(6, name="conv_dw_6_dwrelu"))
+    hde.add(layers.Conv2D(256, (1,1), padding="same", use_bias=False, strides=(1,1), name="conv_dw_6_pwconv2d"))
+    hde.add(layers.BatchNormalization(name="conv_dw_6_pwbn"))
+    hde.add(layers.ReLU(6, name="conv_dw_6_pwrelu"))
+
+    hde.add(layers.DepthwiseConv2D((3,3), strides=(2,2), padding="same"))
+    hde.add(layers.BatchNormalization(name="conv_dw_7_dwbn"))
+    hde.add(layers.ReLU(6, name="conv_dw_7_dwrelu"))
+    hde.add(layers.Conv2D(512, (1,1), padding="same", use_bias=False, strides=(1,1), name="conv_dw_7_pwconv2d"))
+    hde.add(layers.BatchNormalization(name="conv_dw_7_pwbn"))
+    hde.add(layers.ReLU(6, name="conv_dw_7_pwrelu"))
+
+    hde.add(layers.DepthwiseConv2D((3,3), strides=(1,1), padding="same"))
+    hde.add(layers.BatchNormalization(name="conv_dw_8_dwbn"))
+    hde.add(layers.ReLU(6, name="conv_dw_8_dwrelu"))
+    hde.add(layers.Conv2D(512, (1,1), padding="same", use_bias=False, strides=(1,1), name="conv_dw_8_pwconv2d"))
+    hde.add(layers.BatchNormalization(name="conv_dw_8_pwbn"))
+    hde.add(layers.ReLU(6, name="conv_dw_8_pwrelu"))
+
+    hde.add(layers.DepthwiseConv2D((3,3), strides=(2,2), padding="same"))
+    hde.add(layers.BatchNormalization(name="conv_dw_9_dwbn"))
+    hde.add(layers.ReLU(6, name="conv_dw_9_dwrelu"))
+    hde.add(layers.Conv2D(1024, (1,1), padding="same", use_bias=False, strides=(1,1), name="conv_dw_9_pwconv2d"))
+    hde.add(layers.BatchNormalization(name="conv_dw_9_pwbn"))
+    hde.add(layers.ReLU(6, name="conv_dw_9_pwrelu"))
+
+    hde.add(layers.DepthwiseConv2D((3,3), strides=(1,1), padding="same"))
+    hde.add(layers.BatchNormalization(name="conv_dw_10_dwbn"))
+    hde.add(layers.ReLU(6, name="conv_dw_10_dwrelu"))
+    hde.add(layers.Conv2D(1024, (1,1), padding="same", use_bias=False, strides=(1,1), name="conv_dw_10_pwconv2d"))
+    hde.add(layers.BatchNormalization(name="conv_dw_10_pwbn"))
+    hde.add(layers.ReLU(6, name="conv_dw_10_pwrelu"))
+
+    hde.add(layers.DepthwiseConv2D((3,3), strides=(2,2), padding="same"))
+    hde.add(layers.BatchNormalization(name="conv_dw_11_dwbn"))
+    hde.add(layers.ReLU(6, name="conv_dw_11_dwrelu"))
+    hde.add(layers.Conv2D(2048, (1,1), padding="same", use_bias=False, strides=(1,1), name="conv_dw_111_pwconv2d"))
+    hde.add(layers.BatchNormalization(name="conv_dw_11_pwbn"))
+    hde.add(layers.ReLU(6, name="conv_dw_11_pwrelu"))
+
+
+    hde.add(layers.Conv2D(32, (3,3), strides=(1,1), padding="same", name="conv2d_2"))
+    hde.add(layers.Flatten())
+    hde.add(layers.Dense(2, activation="sigmoid", name="out"))
     hde.summary()
 
     return hde
@@ -48,13 +124,16 @@ if __name__ == '__main__':
     model = create_model()
     model.save("net.hdf5")
 
-    np.random.seed(2)
-    x = np.random.rand(1,3,192,192)
-    print(x.shape)
-    r = model.predict(x, batch_size=1)
+    np.random.seed(datetime.datetime.now().microsecond)
+
+    for i in range(10):
+        x = np.random.rand(1,192,192,3)
+        print(x.shape)
+        r = model.predict(x, batch_size=1)
+        print(r)
 
     r = np.array([r])
-    x =  x.transpose(0, 3, 1, 2)
+    #x =  x.transpose(0, 3, 1, 2)
     #r =  r.transpose(0, 3, 1, 2)
     print("in: ", np.shape(x))
     print("out: ", np.shape(r))
@@ -77,10 +156,18 @@ if __name__ == '__main__':
 
     for layer in model.layers:
         data = layer.get_weights()
-        if (len(data) != 0):
-            weights = data[0].flatten()
-            biases = data[1].flatten()
-            f = open("test_deer_hde/layers/" + layer.name + ".bin", mode='wb')
+        if (len(data) > 0):
+                
+            # If this is a batch normalization layer, keep writing to last bin. Else, start new one
+            if isinstance(layer, layers.BatchNormalization):
+                data = data[1:]
+            else:
+                f.close()
+                f = open("test_deer_hde/layers/" + layer.name + ".bin", mode='wb')
+                if not layer.use_bias:
+                    data.append(np.zeros(layer.output_shape[-1]))
 
-            bin_write(f, np.concatenate([d.flatten() for d in data]))
-            f.close()
+            flattened_data = [d.flatten() for d in data]
+            bin_write(f, np.concatenate(flattened_data))
+    
+    f.close()
